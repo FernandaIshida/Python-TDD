c5de3c4f163d378d1947993be46287eee428ae51
Fernanda Ishida <fer.ishida@gmail.com>
2023-05-24 01:24:50 -0300

    Metodo com exception

--- codigo/bytebank.py
+++ codigo/bytebank.py
@@ -29,7 +29,6 @@ class Funcionario:
         sobrenomes = ['Braganca', 'Windsor', 'Bourbon', 'Yamato', 'Al Saud', 'Khan', 'Tudor', 'Ptolomeu']
         return (self._salario >= 100000) and (self.sobrenome() in sobrenomes)
 
-
     def decrescimo_salario(self):
         if self._eh_socio():
             decrescimo = self._salario * 0.1
@@ -38,7 +37,7 @@ class Funcionario:
     def calcular_bonus(self):
         valor = self._salario * 0.1
         if valor > 1000:
-            valor = 0
+            raise Exception('O salario e muito alto para receber um bonus.')
         return valor
 
     def __str__(self):
