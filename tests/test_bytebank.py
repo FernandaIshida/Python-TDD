2b77c3de0063d8c138cf3deec4097d61f9db603e
Fernanda Ishida <fer.ishida@gmail.com>
2023-05-24 14:36:24 -0300

    Testes com markers

--- tests/test_bytebank.py
+++ tests/test_bytebank.py
@@ -1,5 +1,7 @@
 from codigo.bytebank import Funcionario
 import pytest
+from pytest import mark
+
 
 class TestClass:
     def test_quando_idade_recebe_13_03_2000_deve_retornar_o_valor_de_22(self):
@@ -30,9 +32,9 @@ class TestClass:
         resultado = funcionario_teste.salario
 
         assert resultado == esperado
-
+    @mark.calcular_bonus
     def test_quando_calcular_bonus_recebe_1000_deve_retornar_100(self):
-        entrada = 1000 #Given
+        entrada = 1000 # Given
         esperado = 100
 
         funcionario_teste = Funcionario('teste', '11/11/2000', entrada)
@@ -40,6 +42,7 @@ class TestClass:
 
         assert resultado == esperado
 
+    @mark.calcular_bonus
     def test_quando_calcular_bonus_recebe_1000000_deve_retornar_exception(self):
         with pytest.raises(Exception):
             entrada = 1000000 #Given
@@ -47,5 +50,5 @@ class TestClass:
             funcionario_teste = Funcionario('teste', '11/11/2000', entrada)
             resultado = funcionario_teste.calcular_bonus() #When
 
-            assert  resultado #Then
+            assert resultado #Then
 
