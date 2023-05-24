df185afdcfe1d5014f7e020d81c02d7b27a3c6d8
Fernanda Ishida <fer.ishida@gmail.com>
2023-05-24 00:11:15 -0300

    TDD

--- tests/test_bytebank.py
+++ tests/test_bytebank.py
@@ -19,5 +19,13 @@ class TestClass:
 
         assert resultado == esperado #Then
 
+    def test_quando_decrescimo_salario_recebe_100000_deve_retornar_90000(self):
+        entrada_salario = 100000 #Given
+        entrada_nome = 'Paulo Braganca'
+        esperado = 90000
 
+        funcionario_teste = Funcionario(entrada_nome, '11/11/2000', entrada_salario)
+        funcionario_teste.decrescimo_salario() #When
+        resultado = funcionario_teste.salario
 
+        assert resultado == esperado
